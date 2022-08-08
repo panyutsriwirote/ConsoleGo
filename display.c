8b5bc25e5b254d877a561e0248a07aaed608e72d
Panyut Sriwirote <mildpanyut@gmail.com>
2022-08-08 21:08:00 +0700

    update non-windows display

--- display.c
+++ display.c
@@ -2,7 +2,17 @@
 #include <stdio.h>
 
 void show_board() {
-    puts(board);
+    for (int i = 0; i < sizeof(board); i++) {
+        switch (board[i]) {
+            case 'Y':
+            case 'P':
+                putchar(' ');
+                break;
+            default:
+                putchar(board[i]);
+        }
+    }
+    putchar('\n');
     printf("             X's prisoner: %d\n", X_prisoner);
     printf("             O's prisoner: %d\n", O_prisoner);
 }

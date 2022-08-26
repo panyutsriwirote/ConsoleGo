2d236a64c0915cdbb6d734bb0e9cb4cac70337fe
Panyut Sriwirote <mildpanyut@gmail.com>
2022-08-26 18:15:22 +0700

    improve main.c readability

--- src/main.c
+++ src/main.c
@@ -4,7 +4,10 @@
 
 int main() {
     show_cur_board();
-    puts("Enter:\n   (1) a coordinate ([A-I][1-9]) to place a stone\n   (2) P to pass\n   (3) U to undo last move");
+    puts("Enter:");
+    puts("   (1) a coordinate ([A-I][1-9]) to place a stone");
+    puts("   (2) P to pass");
+    puts("   (3) U to undo last move");
     while (true) {play(get_move());}
     return EXIT_SUCCESS;
 }
